0efff831403c5b4b7ca6d06cbcf9044836e08568
arthawanks <arthawan0@gmail.com>
2020-03-04 18:29:34 +0800

    tugas2

---
+++ index.js
@@ -0,0 +1,97 @@
+const electron = require('electron');
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let todayWindow;
+let creteWindow;
+let listWindow;
+
+app.on("ready", () => {
+    todayWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration : true
+        },
+        title : "Aplikasi Dokter"
+    });
+
+    todayWindow.loadURL(`file://${__dirname}/today.html`);
+    todayWindow.on("closed", () => {
+
+        app.quit();
+        todayWindow = null;
+    });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+    
+});
+
+const listWindowCreator = () =>
+    {
+        listWindow = BrowserWindow
+        ({
+            webPreferences:{
+                nodeIntegration: true
+            },
+            width: 600,
+            height: 400,
+            title: "All Appointments"
+        });
+
+        listWindow.setMenu(null);
+        listWindow.loadURL(`file://${__dirname}/list.html`);
+        listWindow.on("closed", () => (listWindow = null));
+    };
+
+const createWindowCreator = () =>
+    {
+        createWindow = BrowserWindow
+        ({
+            webPreferences:{
+                nodeIntegration: true
+            },
+            width: 600,
+            height: 400,
+            title: "Create Appointments"
+        });
+
+        createWindow.setMenu(null);
+        createWindow.loadURL(`file://${__dirname}/create.html`);
+        createWindow.on("closed", () => (createWindow = null));
+    };
+
+const menuTemplate = [{
+    label: "File",
+    submenu: 
+        [
+            {
+                label: "New Appointment",
+
+                click(){
+                createWindowCreator();
+            }
+
+            },
+            { 
+                label:"All Appoinment", 
+                click(){
+                listWindowCreator();
+                }
+            },
+            {
+                label:"Quit",
+                accelerator: process.platform ==="darwin" ? "Command+Q":
+                "Ctrl +Q",
+                click(){
+                app.quit();
+                }
+            }
+        ]
+    },
+    {
+
+        label:"View",
+        submenu: [{ role: "reload "}, { role: "toggledevtools"}]
+
+    }
+]
